b6a059bcd4270c58248178ccc7180af1fb00676a
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2023-11-27 11:11:17 +0200

    Use SoundContext for mute button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,28 @@
-'use client';
-import React from 'react';
-import { Volume2, VolumeX } from 'react-feather';
+"use client";
+import React from "react";
+import { Volume2, VolumeX } from "react-feather";
 
-import VisuallyHidden from '../VisuallyHidden';
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import styles from './Header.module.css';
+import VisuallyHidden from "../VisuallyHidden";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import styles from "./Header.module.css";
+import { SoundContext } from "../SoundProvider/SoundProvider";
 
 function Header() {
   const id = React.useId();
 
+  const { isMuted, toggleMute } = React.useContext(SoundContext);
   // TODO: Global state?
-  const soundEnabled = true;
+  // const soundEnabled = true;
 
   return (
     <header className={styles.wrapper}>
-      <MaxWidthWrapper
-        className={styles.innerWrapper}
-      >
+      <MaxWidthWrapper className={styles.innerWrapper}>
         <a href="/">Kool Website</a>
 
-        <button
-          onClick={() => {
-            // TODO: flip `soundEnabled`
-          }}
-        >
-          {soundEnabled ? (
-            <Volume2 />
-          ) : (
-            <VolumeX />
-          )}
+        <button onClick={toggleMute}>
+          {isMuted ? <VolumeX /> : <Volume2 />}
           <VisuallyHidden>
-            {soundEnabled
-              ? 'Disable sound effects'
-              : 'Enable sound effects'}
+            {isMuted ? "Enable sound effects" : "Disable sound effects"}
           </VisuallyHidden>
         </button>
       </MaxWidthWrapper>
